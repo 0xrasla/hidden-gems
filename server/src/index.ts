271e8a3dd7c8497a483a99c20af3ea5cfe148055
rasla <programmer.rasla@gmail.com>
2024-06-28 00:28:57 +0530

    Waste

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import cors from "@elysiajs/cors";
+// import cors from "@elysiajs/cors";
 import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import { Logestic } from "logestic";
@@ -9,7 +9,7 @@ import baseRouter from "./routes/router";
 const app = new Elysia();
 
 // @ts-ignore
-app.use(cors());
+// app.use(cors());
 
 try {
   await mongoose.connect(process.env.MONGO_URI as string, {
