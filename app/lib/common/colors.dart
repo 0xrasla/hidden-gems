432c770e2d906d5e708f5f558a46557d0c792c03
rasla <programmer.rasla@gmail.com>
2024-07-02 11:26:25 +0530

    PWA TIME

--- app/lib/common/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const accentColor = Color.fromRGBO(57, 57, 57, 1);
-  static const commonColor = Color.fromRGBO(57, 57, 57, 1);
-}
--- app/lib/common/config.dart
+++
@@ -1,6 +0,0 @@
-class AppConfig {
-  static String appTitle = "Green Tech";
-  static String appVersion = "1.0.0";
-
-  static String apiUrl = "http://192.168.1.7:4000/api";
-}
